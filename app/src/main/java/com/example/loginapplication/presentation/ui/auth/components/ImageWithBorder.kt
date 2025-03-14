17bc376061227818d133acc966bee49061009e5f
mian muzammil <mianmuzammil30@gmail.com>
2025-03-14 23:37:53 +0500

    remove unnecessary imports

---
+++ app/src/main/java/com/example/loginapplication/presentation/ui/auth/components/ImageWithBorder.kt
@@ -0,0 +1,35 @@
+package com.example.loginapplication.presentation.ui.auth.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.loginapplication.R
+
+@Composable
+fun ImageWithBorder(image: Int) {
+    Box(
+
+        modifier = Modifier
+            .size(54.dp)
+            .border(1.dp, colorResource(id =R.color.gray), shape = CircleShape),
+
+    ) {
+        Image(
+            painter = painterResource(id = image), // Replace with your image
+            contentDescription = null,
+            modifier = Modifier
+                .size(46.dp) // The image size should be 50dp
+                .padding(1.dp)
+                .align(Alignment.Center)// 1dp space between the image and the border
+        )
+    }
+}
--- app/src/main/java/com/example/loginapplication/presentation/ui/auth/components/TextInputField.kt
+++ app/src/main/java/com/example/loginapplication/presentation/ui/auth/components/TextInputField.kt
@@ -1,20 +1,20 @@
 package com.example.loginapplication.presentation.ui.auth.components
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
 
