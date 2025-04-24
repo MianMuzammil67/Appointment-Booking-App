d9d8515235a0390d933e4d447127fafc0d432e7c
mian muzammil <mianmuzammil30@gmail.com>
2025-04-25 01:29:21 +0500

    handle full & limited doctor categories

--- app/src/main/java/com/example/appointmentbookingapp/presentation/ui/allCategories/AllDoctorCategories.kt
+++ app/src/main/java/com/example/appointmentbookingapp/presentation/ui/allCategories/AllDoctorCategories.kt
@@ -17,9 +17,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,7 +25,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.example.appointmentbookingapp.domain.model.DoctorCategory
 import com.example.appointmentbookingapp.presentation.state.UiState
 import com.example.appointmentbookingapp.presentation.ui.home.components.CategoryItem
 import com.example.appointmentbookingapp.presentation.ui.home.viewModel.HomeViewModel
@@ -39,7 +35,6 @@ fun AllDoctorCategories(navController: NavHostController) {
     val viewMode: HomeViewModel = hiltViewModel()
 
     val allCategories by viewMode.allCategoriesState.collectAsState()
-//    var categoryList by remember { mutableStateOf(emptyList<DoctorCategory>()) }
 
     Scaffold(
         Modifier.fillMaxSize(),
