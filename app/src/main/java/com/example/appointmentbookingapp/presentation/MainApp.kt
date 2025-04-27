c02b78968fec4800456a8271eee2d6ade34ea5bf
mian muzammil <mianmuzammil30@gmail.com>
2025-04-27 23:53:57 +0500

    implement BottomNavigationBar

--- app/src/main/java/com/example/appointmentbookingapp/presentation/MainApp.kt
+++ app/src/main/java/com/example/appointmentbookingapp/presentation/MainApp.kt
@@ -1,9 +1,7 @@
 package com.example.appointmentbookingapp.presentation
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -14,46 +12,63 @@ import com.example.appointmentbookingapp.presentation.ui.appointment.BookAppoint
 import com.example.appointmentbookingapp.presentation.ui.auth.SignInScreen
 import com.example.appointmentbookingapp.presentation.ui.auth.SignupScreen
 import com.example.appointmentbookingapp.presentation.ui.doctorDetail.DocDetailScreen
+import com.example.appointmentbookingapp.presentation.ui.favorite.FavoriteScreen
 import com.example.appointmentbookingapp.presentation.ui.home.HomeScreen
 import com.example.appointmentbookingapp.presentation.ui.home.viewModel.HomeViewModel
 import com.example.appointmentbookingapp.presentation.ui.home.viewModel.SharedDoctorViewModel
+import com.example.appointmentbookingapp.presentation.ui.navigation.BottomNavigationBar
 import com.google.firebase.auth.FirebaseAuth
+
 @Composable
 fun MainApp() {
-    Surface (modifier = Modifier.fillMaxSize()){
-        val navController = rememberNavController()
+//    Surface (modifier = Modifier.fillMaxSize()){
+    val navController = rememberNavController()
 
-        val sharedDoctorViewModel: SharedDoctorViewModel = viewModel()
-        val homeViewModel: HomeViewModel = viewModel()
+    val sharedDoctorViewModel: SharedDoctorViewModel = viewModel()
+    val homeViewModel: HomeViewModel = viewModel()
 
-        val currentUser = FirebaseAuth.getInstance().currentUser
-        val start = if (currentUser != null) "HomeScreen" else "SignIn"
+    val currentUser = FirebaseAuth.getInstance().currentUser
+    val start = if (currentUser != null) "HomeScreen" else "SignIn"
 
-        NavHost(navController = navController, startDestination = start){
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(navController = navController)
+        },
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = start,
+//            Modifier.padding(innerPadding)
 
-            composable("SignUp"){
+
+        ) {
+
+//        NavHost(navController = navController, startDestination = start){
+
+            composable("SignUp") {
                 SignupScreen(navController)
             }
-            composable("SignIn"){
+            composable("SignIn") {
                 SignInScreen(navController)
             }
-            composable("HomeScreen"){
-                HomeScreen(navController,sharedDoctorViewModel,homeViewModel)
+            composable("HomeScreen") {
+                HomeScreen(navController, sharedDoctorViewModel, homeViewModel)
             }
-            composable("DoctorDetail"){
-                DocDetailScreen(navController,sharedDoctorViewModel)
+            composable("DoctorDetail") {
+                DocDetailScreen(navController, sharedDoctorViewModel)
             }
-            composable("BookAppointment"){
-                BookAppointmentScreen(navController,sharedDoctorViewModel)
+            composable("BookAppointment") {
+                BookAppointmentScreen(navController, sharedDoctorViewModel)
             }
-            composable("AllDoctorCategories"){
-                AllDoctorCategories(navController,homeViewModel)
+            composable("AllDoctorCategories") {
+                AllDoctorCategories(navController, homeViewModel)
             }
-            composable("DoctorScreen"){
-                DoctorScreen(navController,homeViewModel,sharedDoctorViewModel)
+            composable("DoctorScreen") {
+                DoctorScreen(navController, homeViewModel, sharedDoctorViewModel)
             }
-
+            composable("FavoriteScreen") {
+                FavoriteScreen(navController)
+            }
         }
-
     }
 }
--- app/src/main/java/com/example/appointmentbookingapp/presentation/ui/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/example/appointmentbookingapp/presentation/ui/navigation/BottomNavigationBar.kt
@@ -1,4 +1,54 @@
 package com.example.appointmentbookingapp.presentation.ui.navigation
 
-class BottomNavigationBar {
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    val items = listOf(
+        NavigationItem("Home", Icons.Filled.Home, "HomeScreen"),
+        NavigationItem("Categories", Icons.Filled.List, "AllDoctorCategories"),
+        NavigationItem("Profile", Icons.Filled.Person, "FavoriteScreen")
+    )
+    NavigationBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        items.forEach { item ->
+            NavigationBarItem(
+                icon = { Icon(item.icon, contentDescription = item.title) },
+                label = { Text(item.title) },
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
 }
+
+data class NavigationItem(val title: String, val icon: ImageVector, val route: String)
