70bd47449b3af0d0eb7f7d0ecb0a5f634eacb403
mian muzammil <mianmuzammil30@gmail.com>
2025-06-12 23:58:17 +0500

    remove unnecessary comments and reformat code

--- app/src/main/java/com/example/appointmentbookingapp/presentation/ui/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/example/appointmentbookingapp/presentation/ui/favorite/FavoriteScreen.kt
@@ -3,7 +3,6 @@ package com.example.appointmentbookingapp.presentation.ui.favorite
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -25,9 +24,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.appointmentbookingapp.domain.model.DoctorItem
 import com.example.appointmentbookingapp.presentation.state.UiState
 import com.example.appointmentbookingapp.presentation.ui.components.DocCard
@@ -37,103 +38,7 @@ import com.example.appointmentbookingapp.presentation.ui.favorite.viewModel.Favo
 @Composable
 fun FavoriteScreen(navController: NavController) {
     val favoriteViewModel: FavoriteViewModel = hiltViewModel()
-
     val favoriteDoctorsState by favoriteViewModel.favoritesState.collectAsState()
-
-
-//    val doctorList = listOf(
-//        DoctorItem(
-//            id = "doc_001",
-//            name = "Dr. Amelia Carter",
-//            aboutDoctor = "Expert in women's health and gynecology with 12 years of experience.",
-//            imageUrl = "https://img.freepik.com/free-photo/female-doctor-hospital-with-stethoscope_23-2148827774.jpg",
-//            rating = "4.8",
-//            docCategory = "Gynecologist",
-//            isFavorite = false,
-//            experienceYears = 12,
-//            consultationFee = "$80",
-////                isOnlineAvailable = true,
-//            languagesSpoken = listOf("English", "Spanish"),
-//            gender = "Female",
-//            reviewsCount = 150
-//        ),
-//        DoctorItem(
-//            id = "doc_002",
-//            name = "Dr. Nathan Kim",
-//            aboutDoctor = "Cardiologist with a patient-first approach and over a decade of experience.",
-//            imageUrl = "https://www.hjhospitals.org/photos/doctors/_MG_1223t.jpg",
-//            rating = "4.6",
-//            docCategory = "Cardiologist",
-//            isFavorite = false,
-//            experienceYears = 11,
-//            consultationFee = "$100",
-////        isOnlineAvailable = false,
-//            languagesSpoken = listOf("English", "Korean"),
-//            gender = "Male",
-//            reviewsCount = 200
-//        ),
-//        DoctorItem(
-//            id = "doc_003",
-//            name = "Dr. Priya Mehta",
-//            aboutDoctor = "Pediatrician who makes kids feel at ease with compassionate care.",
-//            imageUrl = "https://imgcdn.stablediffusionweb.com/2024/11/8/7446cd96-3e65-46fe-9cf3-5b096275ad49.jpg",
-//            rating = "4.9",
-//            docCategory = "Pediatrician",
-//            isFavorite = false,
-//            experienceYears = 9,
-//            consultationFee = "$70",
-////        isOnlineAvailable = true,
-//            languagesSpoken = listOf("English", "Hindi", "Gujarati"),
-//            gender = "Female",
-//            reviewsCount = 180
-//        ),
-//        DoctorItem(
-//            id = "doc_004",
-//            name = "Dr. Daniel Wu",
-//            aboutDoctor = "Dermatologist helping patients with skincare and advanced treatments.",
-//            imageUrl = "https://jerrysusa.com/wp-content/uploads/2014/11/doctor-profile-02-250x375.jpg",
-//            rating = "4.7",
-//            docCategory = "Dermatologist",
-//            isFavorite = false,
-//            experienceYears = 10,
-//            consultationFee = "$90",
-////        isOnlineAvailable = true,
-//            languagesSpoken = listOf("English", "Mandarin"),
-//            gender = "Male",
-//            reviewsCount = 130
-//        ),
-//        DoctorItem(
-//            id = "doc_005",
-//            name = "Dr. Sara Thompson",
-//            aboutDoctor = "Orthopedic specialist with deep expertise in joint and bone care.",
-//            imageUrl = "https://adaptcommunitynetwork.org/wp-content/uploads/2023/09/person-placeholder-500x367.jpg",
-//            rating = "4.5",
-//            docCategory = "Orthopedic",
-//            isFavorite = false,
-//            experienceYears = 8,
-//            consultationFee = "$85",
-////        isOnlineAvailable = false,
-//            languagesSpoken = listOf("English"),
-//            gender = "Female",
-//            reviewsCount = 120
-//        ),
-//        DoctorItem(
-//            id = "doc_006",
-//            name = "Dr. Ahmed Al-Farsi",
-//            aboutDoctor = "Neurologist known for detailed diagnosis and patient-centric care.",
-//            imageUrl = "https://adaptcommunitynetwork.org/wp-content/uploads/2023/09/person-placeholder-500x367.jpg",
-//            rating = "4.6",
-//            docCategory = "Neurologist",
-//            isFavorite = false,
-//            experienceYears = 15,
-//            consultationFee = "$110",
-////        isOnlineAvailable = true,
-//            languagesSpoken = listOf("English", "Arabic"),
-//            gender = "Male",
-//            reviewsCount = 210
-//        )
-//    )
-//    val favoriteDoctor = doctorList.filter { it.isFavorite }
 
     Scaffold(
         topBar = {
@@ -159,7 +64,7 @@ fun FavoriteScreen(navController: NavController) {
                 is UiState.Loading -> {
                     Box(
                         modifier = Modifier
-                            .fillMaxWidth()
+                            .fillMaxSize()
                             .padding(vertical = 16.dp),
                         contentAlignment = Alignment.Center
                     ) {
@@ -195,13 +100,12 @@ fun FavoriteScreen(navController: NavController) {
 
             }
 
-
-//            items(favoriteDoctorsState.value) { doctor ->
-//                Spacer(modifier = Modifier.height(8.dp))
-//                DocCard(doctor = doctor) {}
-//            }
-
         }
 
     }
+}
+@Preview
+@Composable
+fun FavoriteScreenPreview(){
+    FavoriteScreen(navController = rememberNavController())
 }
--- app/src/main/java/com/example/appointmentbookingapp/presentation/ui/favorite/viewModel/FavoriteViewModel.kt
+++ app/src/main/java/com/example/appointmentbookingapp/presentation/ui/favorite/viewModel/FavoriteViewModel.kt
@@ -5,11 +5,13 @@ import androidx.lifecycle.viewModelScope
 import com.example.appointmentbookingapp.domain.model.DoctorItem
 import com.example.appointmentbookingapp.domain.repository.FavoriteRepository
 import com.example.appointmentbookingapp.presentation.state.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class FavoriteViewModel @Inject constructor(
     private val favoriteRepository: FavoriteRepository
 ) : ViewModel() {
